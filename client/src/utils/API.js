ce19743d7d97c604a54f24c9c701f384429f99b2
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-08-20 23:58:45 -0500

    updated login css, working on cors

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,11 @@
 import axios from "axios";
-const BASEURL = "https://www.hikingproject.com/data/get-trailsby-id?ids=7001635,7002742,7000108,7002175,7000130";
+const BASEURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10";
 const APIKEY = "&key=200559603-3fbad4d84fcb99d1ee9432d2fcd6f2da";
 
 export default {
-  search: function(query) {
+  search: function() {
     return axios.get(BASEURL + APIKEY);
   }
 };
+
+
