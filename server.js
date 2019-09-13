e896034200906a313925a0d297670d5c29e701dd
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-09-12 23:59:22 -0500

    added env

--- server.js
+++ server.js
@@ -29,19 +29,6 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://www.hikingproject.com"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// app.get('/', function(req, res, next) {
-//   // Handle the get for this route
-// });
-
-// app.post('/', function(req, res, next) {
-//  // Handle the post for this route
-// });
 
 const port = process.env.PORT || 5000;
 
